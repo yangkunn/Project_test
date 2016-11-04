568af4de5cd0acc1e37ece6facbf8fa58b128938
yangkunn <yanghs9748@naver.com>
2016-11-04 09:17:43 +0900

    Add sum function

--- calc.cpp
+++ calc.cpp
@@ -1,8 +1,18 @@
 #include <iostream>
 using namespace std;
 
+int sum(int a, int b);
+
 int main()
 {
 	cout << "Init" << endl;
+
+	cout << sum(4, 3) << endl;
+
 	return 0;
+}
+
+int sum(int a, int b)
+{
+	return a+b;
 }
