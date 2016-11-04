726df4d3d474f7501530340d70095a8a45768505
yangkunn <yanghs9748@naver.com>
2016-11-04 09:06:40 +0900

    Add sub function

--- function.cpp
+++ function.cpp
@@ -1,14 +1,21 @@
 #include <stdio.h>
 
 int sum(int a, int b);
+int sub(int a, int b);
 
 int main()
 {
 	cout << sum(5, 6) << endl;
+	cout << sub(5, 6) << endl;
 	return 0;
 }
 
 int sum(int a, int b)
 {
 	return a + b;
+}
+
+int sub(int a, int b)
+{
+	return a - b;
 }
